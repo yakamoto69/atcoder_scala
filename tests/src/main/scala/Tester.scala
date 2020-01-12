54f45f8abfb9df93585321a9367ee7a9d9a8df97
飯野　健二 <yakamoto@mac.com>
2020-01-12 20:55:44 +0900

    Tester変更

--- tests/src/main/scala/Tester.scala
+++ tests/src/main/scala/Tester.scala
@@ -5,33 +5,27 @@ import scala.io.Source
 
 object Tester {
 
-  case class TestCase(in: String, out: String)
+  case class TestCase(name: String, in: String, out: String)
 
   def readTestCases(name: String): Seq[TestCase] = {
-    var i = 0
-    val in = Array.fill[ArrayBuffer[String]](2)(ArrayBuffer())
     val tests = ArrayBuffer[TestCase]()
 
-    def addTest(): Unit = {
-      tests += TestCase(in(0).mkString("\n"), in(1).mkString("\n"))
-      in foreach (_.clear())
+    val file = getClass.getClassLoader.getResource(s"$name.txt")
+    val txt = Source.fromURL(file).mkString
+    var pos = 0
+    while(txt != null) {
+      val inStart = txt.indexOf("\uD83D\uDE3Ain", pos)
+      if (inStart == -1) return tests
+      val outStart = txt.indexOf("\uD83D\uDE3Aout", inStart)
+      if (outStart == -1) throw new IllegalStateException("形式がおかしいです")
+      val end = txt.indexOf("\uD83D\uDE3Aend", outStart)
+      if (end == -1) throw new IllegalStateException("形式がおかしいです")
+      val name = txt.substring(inStart+5, txt.indexOf('\n', inStart))
+      val in = txt.substring(txt.indexOf('\n', inStart)+1, outStart)
+      val out = txt.substring(txt.indexOf('\n', outStart)+1, end)
+      tests += TestCase(name, in, out)
+      pos = end+6
     }
-
-    val file = new File(getClass.getClassLoader.getResource(s"$name.txt").getFile)
-    val lines = Source.fromFile(file).getLines().toList
-    lines foreach { l =>
-      if (l.isEmpty || l.startsWith("//")) {
-        if (in(i).nonEmpty) {
-          i ^= 1
-          if (i == 0) addTest()
-        }
-      } else {
-        in(i) += l
-      }
-    }
-
-    if (i == 0) throw new IllegalStateException("outで終わっていない")
-    else addTest()
 
     tests
   }
@@ -41,7 +35,7 @@ object Tester {
       import java.io.PrintWriter
 
       import Main.InputReader
-      println(s"Test${i+1}")
+      println(s"[${test.name}]")
 
       val out = new ByteArrayOutputStream()
       val in = new ByteArrayInputStream(test.in.getBytes("utf-8"))
